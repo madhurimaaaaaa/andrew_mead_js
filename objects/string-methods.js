9788b42b88221b0bb56d52f48cf59dbc6c37fb27
Madhurima Kayal <madhurimakayal97@gmail.com>
2023-07-19 14:48:56 -0700

    Done

---
+++ objects/string-methods.js
@@ -0,0 +1,30 @@
+let name = '  Madhurima K'
+//length property
+console.log(name.length)
+//convert to uppercase
+console.log(name.toUpperCase())
+
+// c to lower case
+console.log(name.toLowerCase())
+// includes
+let password = 'abcd1234'
+console.log(password.includes('password'))
+
+//trim
+console.log(name.trim())
+
+//challenge
+let isValidPassword = function (password) {
+    if (password.length > 8 && password.includes) {
+        return true 
+    }
+        else {
+            return false
+        }
+    }
+    
+
+console.log(isValidPassword('abscd'))
+console.log(isValidPassword('aabcdfgkkkkk00l'))
+console.log(isValidPassword('abscdhhh65password#$'))
+
