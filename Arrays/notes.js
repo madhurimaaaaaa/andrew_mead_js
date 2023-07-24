c5f3d8cdc93fdc3b9c42966bb33dd9dc6f1dfa59
Madhurima Kayal <madhurimakayal97@gmail.com>
2023-07-24 14:16:09 -0700

    L And C both done

---
+++ Arrays/notes.js
@@ -0,0 +1,9 @@
+const notes = ['Note 1', 'Note 2', 'Note 3']
+console.log(notes.length)
+console.log(notes[notes.length - 3])
+
+
+const todos = ['Eat breakfast', 'Clean house', 'Cook lunch', 'Do gym', 'Do coding']
+console.log(`You have ${todos.length} todos!`)
+console.log(`Todo : ${todos[0]}`)
+console.log(`Todo : ${todos[todos.length - 2]}`)
