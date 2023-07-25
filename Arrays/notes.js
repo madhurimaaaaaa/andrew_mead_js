ac39ec3107fac2aca4129c0b8136f3e0d1e9a10f
Madhurima Kayal <madhurimakayal97@gmail.com>
2023-07-25 16:55:53 -0700

    C done

--- Arrays/notes.js
+++ Arrays/notes.js
@@ -1,5 +1,5 @@
-const notes = ['Note 1', 'Note 2', 'Note 3']
-console.log(notes.pop())
+const notes = ['Note 1', 'Note 2', 'Note 3', 'note 4']
+//console.log(notes.pop())
 //notes.push('My new note')
 
 //console.log(notes.shift())
@@ -13,5 +13,12 @@ console.log(notes.pop())
 //console.log(notes[notes.length - 3])
 console.log(notes)
 
+for (let count = 0; count <= 3; count++) {
+    console.log(count)
+}
+
+for (let count = notes.length - 1; count >= 0; count--) {
+    console.log(notes[count])
+}
 
 
--- Arrays/todo.js
+++ Arrays/todo.js
@@ -7,4 +7,14 @@ todos.shift()
 
 
 console.log(`You have ${todos.length} todos!`)
-console.log(todos)
+
+todos.forEach(function (todos, index) {
+    const num = index + 1
+    console.log(`${num}. ${todos}`)
+})
+
+for (let count = 3; count < todos.length; count++) {
+    const num = count + 1
+    const todo = todos[count]
+    console.log(`${num}. ${todo}`)
+}
